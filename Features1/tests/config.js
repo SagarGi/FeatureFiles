bb8ede5e396d3437497b11f9ff53f3e1a21cb4be
samikshapaudel <samikshapaudel22@gmail.com>
2022-10-19 12:39:48 +0545

    unwanted comment deleted

--- Features1/tests/config.js
+++ Features1/tests/config.js
@@ -1,5 +1,6 @@
 const { Before, After, AfterAll, BeforeAll } = require("@cucumber/cucumber");
 const { chromium } = require("playwright");
+
 BeforeAll(async () => {
   global.browser = await chromium.launch({ headless: false });
 });
@@ -18,17 +19,3 @@ AfterAll(async () => {
   await browser.close();
 });
 
-// Before(function () {
-//   console.log("This hook runs before a scenario");
-// });
-// After(function () {
-//   console.log("This after hook runs after a scenario");
-// });
-
-// BeforeAll(function () {
-//   console.log("This BeforeAll runs before all scenario");
-// });
-
-// AfterAll(function () {
-//   console.log("This hook runs after all scenario");
-// });
